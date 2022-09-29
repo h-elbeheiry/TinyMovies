b038916a659e08f1e2f9c2e4cf32dd62bcee018e
Hussein Elbeheiry <hussein@elbeheiry.com>
2022-09-29 11:51:11 -0400

    Initial Commit

---
+++ TinyMovies/TinyMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyMoviesApp.swift
+//  TinyMovies
+//
+//  Created by Hussein Elbeheiry on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct TinyMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
